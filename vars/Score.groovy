7dbdd50ff9600e4252feaf5093baf391066776d4
suneelpunnana <suneelpunnana3@gmail.com>
2020-03-09 11:44:37 +0530

    Update Score.groovy

--- vars/Score.groovy
+++ vars/Score.groovy
@@ -67,8 +67,15 @@ def total=jsonObjb.Bamboo.totalBuilds
 	    
     }
 	  
-    
-      
+    if(jsonStringa[i].contains("Sonar"))
+    {
+	    name="Sonar"
+	    for(i=0;i<jsonObj.Sonar.Metrics.component.measures.size();i++){
+    def metric=jsonObj.Sonar.Metrics.component.measures[i].metric
+		    def d=jsonObj.Sonar.Metrics.component.measures[i].value
+    double data = Double.parseDouble(d); 
+       LIST.add(["toolName":name,"metric":metric,"value":data])
+	    }
 	   // metric="successfulbuilds"
     //  def jsonStringb = bamboo
 
